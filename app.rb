f9b79c79d6e20573208d0383f05183cb9158cf2a
Nicola Cardinale <ncardinale@facilitylive.com>
2020-02-27 16:08:03 +0100

    fixed build

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
   end
 
   use Rack::Auth::Basic, "Protected Area" do |username, password|
-    username == 'admin' && password == 'facility'
+    username == 'admin' && password == 'password'
 end
 
   use Rack::Cors do
